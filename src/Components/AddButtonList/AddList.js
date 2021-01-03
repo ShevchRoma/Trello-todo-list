78135d5532d3798070b2012a47e483953fcda42b
Roman Shevchenko <65279235+ShevchRoma@users.noreply.github.com>
2021-01-03 13:25:54 +0200

    Update AddList.js

--- src/Components/AddButtonList/AddList.js
+++ src/Components/AddButtonList/AddList.js
@@ -55,8 +55,8 @@ const AddList = ({colors,onAdd}) =>{
     items ={[{
         className: 'list__add-button',
                icon: <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                     <path d="M8 1V15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                     <path d="M1 8H15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                     <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                     <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                      </svg>
                ,
                name:'Добавить'
